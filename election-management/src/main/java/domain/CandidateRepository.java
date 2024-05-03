6a0a2eacd64d46559bdf0fbe8946a98b514bf7d3
rsbastos <renedossantosbastos@gmail.com>
2024-05-03 11:35:23 -0300

    :white_check_mark: implements findByName test - all tests ok

--- election-management/src/main/java/domain/CandidateRepository.java
+++ election-management/src/main/java/domain/CandidateRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface CandidateRepository {
-
     void save(List<Candidate> candidates);
-
     default void save(Candidate candidate) {
         save(List.of(candidate));
     }
@@ -18,8 +16,7 @@ public interface CandidateRepository {
         return find(new CandidateQuery.Builder().build());
     }
 
-    default Optional<Candidate> findById(String id) {      
-        CandidateQuery query = new CandidateQuery.Builder().ids(Set.of(id)).build();
-        return find(query).stream().findFirst();
+    default Optional<Candidate> findById(String id) {
+        return find(new CandidateQuery.Builder().ids(Set.of(id)).build()).stream().findFirst();
     }
 }
---
+++ election-management/src/test/java/domain/CandidateRepositoryTest.java
@@ -0,0 +1,56 @@
+package domain;
+
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+public abstract class CandidateRepositoryTest {
+    public abstract CandidateRepository repository();
+
+    @Test
+    void save() {
+        Candidate candidate = Instancio.create(Candidate.class);
+        repository().save(candidate);
+
+        Optional<Candidate> result = repository().findById(candidate.id());
+
+        assertTrue(result.isPresent());
+        assertEquals(candidate, result.get());
+    }
+
+    @Test
+    void findAll() {
+        List<Candidate> candidates = Instancio.stream(Candidate.class).limit(10).toList();
+
+        repository().save(candidates);
+
+        List<Candidate> result = repository().findAll();
+
+        assertEquals(result.size(), candidates.size());
+    }
+
+    @Test
+    void findByName() {
+        Candidate candidate1 = Instancio.create(Candidate.class);
+        Candidate candidate2 = Instancio.of(Candidate.class)
+                .set(field("familyName"), "Bastos").create();
+
+        CandidateQuery query = new CandidateQuery.Builder().name("BAS").build();
+
+        repository().save(List.of(candidate1, candidate2));
+
+        List<Candidate> result = repository().find(query);
+
+        assertEquals(1, result.size());
+        assertEquals(candidate2, result.get(0));
+    }
+
+  
+
+}
